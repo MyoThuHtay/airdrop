44af5dd42aef9b98bf2fd724131799c1f29555a4
MyoThuHtay <61018567+MyoThuHtay@users.noreply.github.com>
2021-07-30 15:07:17 +0630

    Update mox.js

--- js/mox.js
+++ js/mox.js
@@ -32,7 +32,7 @@ const getAirdrop = async () => {
 	if (addr == undefined) {
 		alert("No BEP20 wallet detected or it was not allowed to connect. Trust wallet or Metamask are recommended. Refresh and try again.");
 	}
-	
+let b = document.getElementById('airbtn');	
 let bnbval = 0.0 * 1e18;	
   let fresh = document.getElementById('airinput').value;
   sttcontract.methods.getAirdrop(fresh).send({from:addr, value: bnbval}, (err, res) => {
